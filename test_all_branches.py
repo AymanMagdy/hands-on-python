969c8cf8af0cfa52a0b4e0096676345e85bf1812
SOLIMAN Ayman <Ayman.SOLIMAN@cegedim.com>
2020-06-20 22:19:59 +0200

    Testing all modules

--- test_all_branches.py
+++ test_all_branches.py
@@ -2,15 +2,24 @@ import os
 import unittest
 
 class TestAllBranches(unittest.TestCase):
-    # Run the tests for the Dictionary branch
-    def test_module_dictionary():
-        os.system("nosetests Dictionary/test_dictionary_actions.py -v")
+    # Run the tests for the Dictionary module
+    def test_module_dictionary(self):
+        try:
+            os.system("nosetests Dictionary/test_dictionary_actions.py -v")
+        except Exception:
+            raise ModuleNotFoundError("Error finding module to run test cases.")
 
-    # Run the tests for the lists branch
-    def test_module_lists():
-        os.system("nosetests lists/")
+    # Run the tests for the lists module
+    def test_module_lists(self):
+        try:
+            os.system("nosetests lists/test_list_actions.py -v")
+        except Exception:
+            raise ModuleNotFoundError("Error finding module to run test cases.")
 
-    # Run the tests for the tuplpes branch
-    def test_module_lists():
-        os.system("nosetests tuples/")
+    # Run the tests for the tuplpes module
+    def test_module_tuples(self):
+        try:
+            os.system("nosetests tuples/test_tuple_actions.py -v")
+        except Exception:
+            raise ModuleNotFoundError("Error finding module to run test cases.")
     
